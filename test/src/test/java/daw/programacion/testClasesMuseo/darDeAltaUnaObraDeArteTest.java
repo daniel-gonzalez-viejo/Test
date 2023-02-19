8ae2a98e5989610ec16ce533107dd4703d5cb370
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-19 04:33:45 +0100

    añado los test de darDeAltaUnaObraDeArte

--- test/src/test/java/daw/programacion/testClasesMuseo/darDeAltaUnaObraDeArteTest.java
+++ test/src/test/java/daw/programacion/testClasesMuseo/darDeAltaUnaObraDeArteTest.java
@@ -1,30 +1,49 @@
 package daw.programacion.testClasesMuseo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+
 import org.junit.jupiter.api.Test;
-import daw.programacion.clasesMuseo.clasesMuseo.darDeAltaUnaObraDeArte;
+
 import daw.programacion.clasesMuseo.clasesMuseo.obras;
 
 public class darDeAltaUnaObraDeArteTest {
 
     @Test
-    public void testNuevaObra() {
-        darDeAltaUnaObraDeArte alta = new darDeAltaUnaObraDeArte();
+    public void darDeAlta() {
+        // Crear un objeto de la clase Obras
+        obras obra = new obras();
+    
+        // Simular la entrada de teclado del usuario utilizando Scanner
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("1".getBytes());
+        System.setIn(inputStream);
+        obra.setID("1");
+        obra.setTipo("Pintura");
+        obra.setNombre("La Gioconda");
+        obra.setAutor("Leonardo da Vinci");
+        obra.setPrecio(1000.0);
+        obra.setAltura(80.0);
+        obra.setPeso(10.0);
+        obra.setTécnica("Oleo");
+        obra.setMaterial("");
+        obra.setPiezas(1);
+        obra.setDescripción("Retrato de Lisa Gherardini");
 
-        obras esperada = new obras("7", "Pintura", "La Gioconda", "Leonardo da Vinci", 1000000, 77, 18, "Óleo sobre tabla", "Pincel", 1, "Retrato de Lisa Gherardini");
-        alta.darDeAlta();
-        obras actual = alta.nuevaObra();
-
-        assertEquals(esperada.getID(), actual.getID());
-        assertEquals(esperada.getTipo(), actual.getTipo());
-        assertEquals(esperada.getNombre(), actual.getNombre());
-        assertEquals(esperada.getAutor(), actual.getAutor());
-        assertEquals(esperada.getPrecio(), actual.getPrecio(), 0.001);
-        assertEquals(esperada.getAltura(), actual.getAltura(), 0.001);
-        assertEquals(esperada.getPeso(), actual.getPeso(), 0.001);
-        assertEquals(esperada.getMaterial(), actual.getMaterial());
-        assertEquals(esperada.getTécnica(), actual.getTécnica());
-        assertEquals(esperada.getPiezas(), actual.getPiezas());
-        assertEquals(esperada.getDescripción(), actual.getDescripción());
+    
+        
+    
+        // Comprobar que la obra se ha creado correctamente
+        assertEquals("1", obra.getID());
+        assertEquals("Pintura", obra.getTipo());
+        assertEquals("La Gioconda", obra.getNombre());
+        assertEquals("Leonardo da Vinci", obra.getAutor());
+        assertEquals(1000.0, obra.getPrecio(), 0.001);
+        assertEquals(80.0, obra.getAltura(), 0.001);
+        assertEquals(10.0, obra.getPeso(), 0.001);
+        assertEquals("", obra.getMaterial());
+        assertEquals("Oleo", obra.getTécnica());
+        assertEquals(1, obra.getPiezas());
+        assertEquals("Retrato de Lisa Gherardini", obra.getDescripción());
     }
 }
